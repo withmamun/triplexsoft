3b6c1d72b85fd40996002ba566cf510ec04f8d89
Md. Mamun Mia <mamunahammedco59@gmail.com>
2023-08-21 15:24:26 +0600

    third commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,6 +63,31 @@
         })
 
 
+        //------------ Service slider -------------
+
+        $('.service__main__slider').owlCarousel({
+            loop: true,
+            margin: 20,
+            nav: false,
+            dot: true,
+            autoplay: true,
+            smartSpeed: 1000,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+        })
+
+
 
         //--- Venobox service -----
 
